3529881ce86f2f5597a33def67f333d535bb57f5
juli03-droid u <passaupo50@gmail.com>
2024-08-08 15:52:02 +0000

    feat: initiated the project

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x10075AE2666602E6eebbf0E55690a108819858e3'
+export const ProxyAdmin = '0x21560478281FCBd11c3a6dAE34C14d55e8F43aF5'
+export const SWTRImplementation = '0x2380dF343844B4bda805a861D9a789B4f0e5C347'
     
